b8d99ea4032ddd5efb068e9f8848d885b92cd42d
Cristo Sparda <cristo.n6-@hotmail.com>
2024-02-24 17:23:23 -0600

    changes in the add product view

--- CRUD/agregaProd.php
+++ CRUD/agregaProd.php
@@ -12,28 +12,39 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <a href="../inventario.php"><button>Regresar</button></a>
     <form class="contenedor" action="../logic/insertaProducto.php" method="POST" enctype="multipart/form-data" >
-        <div class="col">
+        <a  style="margin: 8px;" href="../inventario.php"><button class="btn"><img src="../icons/arrow-left.svg" alt="">Regresar</button></a>
+        <div class="col d-flex flex-column align-items-center justify-content-center">
             <h1>Agrega un producto</h1>
+            <br>
             <img src="" class="entrada" id="ImagenProducto" alt="">
             <input id="imagen" type="file" name="imagen">
         </div>
-        <div class="col">
+        <div class="col d-flex flex-column align-items-center justify-content-center">
             <span>Ingresar nombre</span>
-            <input type="text" name="nombre">
+            <input class="texto" type="text" name="nombre">
+            <br>
             <span>Ingresar ID</span>
-            <input type="number" name="id">
+            <input class="texto" type="number" name="id">
+            <br>
             <span>Ingresar categoria</span>
-            <input type="text" name="categoria">
+            <input class="texto" type="text" name="categoria">
         </div>
-        <div class="col" style="flex-direction: column;">
+        <div class="col d-flex flex-column align-items-center justify-content-center">
             <span>Ingresar Precio</span>
-            <input type="text" name="precio">
+            <input class="texto" type="text" name="precio">
+            <br>
             <span>Cantidad a ingresar</span>
-            <input type="number" name="cantidad">
-            <button type="submit">Ingresar</button>
-            <button type="button">Cancelar</button>
+            <input class="texto" type="number" name="cantidad">
+            <br>
+            <div class="row">
+                <div class="col">
+                    <button class="btn btn-primary" type="submit">Ingresar</button>
+                </div>
+                <div class="col">
+                    <button class="btn btn-danger" type="button">Cancelar</button>
+                </div>
+            </div>
         </div>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -6,24 +6,25 @@
         <title>Login</title>
         
         <link href="style.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     </head>
 
 
     <body>
         <div class="contenedor">
-            <div class="col" style="width: 40%">
+            <div class="columna" style="width: 60%">
                 <img class="loginImage" src="./Images/loginImage.jpg" alt="">
             </div>
-            <div class="col" style="width: 60%; padding: 16px; justify-content:center;">
+            <div class="columna" style="width: 40%; padding: 16px; justify-content:center;">
                 <h1>Iniciar Sesión</h1>
                 <form action="./logic/Login.php" class="d-flex" method="POST" style="flex-direction: column; gap: 8px;">
                     <label for="">Correo/Nombre</label>
                     <input type="text" name="usuario" placeholder="Ingresar Nombre">
                     <label for="">Contraseña</label>
                     <input type="password" name="contraseña" placeholder="Ingresar contraseña">
-                    <button>Ingresar</button>
+                    <button class="btn-primary">Ingresar</button>
                     <?php 
                         if(isset($_GET['error'])){
                             echo "<span style='color: white; background-color: red; padding: 8px; border-radius: 8px;'> " . $_GET['error'] . "</span>";
