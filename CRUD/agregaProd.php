d2201c3766fa6d4de944edae6d8c015345a134a9
Cristo Sparda <cristo.n6-@hotmail.com>
2024-03-04 09:14:06 -0600

    dropdown button first correction

--- CRUD/agregaProd.php
+++ CRUD/agregaProd.php
@@ -31,7 +31,7 @@
             <input class="texto" type="number" name="id">
             <br>
             <div class="dropdown" name="categoria">
-                <button class="btn btn-succes dropdown-toggle" type="button" data-bs-toggle="dropdown" >
+                <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" >
                     Seleccionar categorias
                 </button>
                 <ul class="dropdown-menu">
@@ -62,6 +62,9 @@
             </div>
         </div>
     </form>
+    <!-- Incluye Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <!-- Boostrap JS  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
